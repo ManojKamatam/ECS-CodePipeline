08858e810b06133e8e930c3225efb969893afb99
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2025-01-16 16:51:09 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,4 @@
-# gunicorn.conf.py
 import multiprocessing
-import os
 
 # Server socket
 bind = "0.0.0.0:5000"
@@ -8,33 +6,17 @@ backlog = 2048
 
 # Worker processes
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = 'sync'
-worker_connections = 1000
+worker_class = "sync"
 timeout = 30
 keepalive = 2
 
 # Logging
-errorlog = '-'
-loglevel = 'debug'
-accesslog = '-'
+errorlog = "-"
+loglevel = "debug"
+accesslog = "-"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
-# Process naming
-proc_name = 'gunicorn_flask'
-
-# Server mechanics
-daemon = False
-pidfile = None
-umask = 0
-user = None
-group = None
-tmp_upload_dir = None
-
-# SSL
-keyfile = None
-certfile = None
-
-# Hook application
+# Server hooks
 def when_ready(server):
     server.log.info("Server is ready. Spawning workers")
 
